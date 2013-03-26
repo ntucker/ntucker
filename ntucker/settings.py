844be0e9cd83e17c502f6494320b3fc4024eb3cf
Nathaniel Tucker <me@ntucker.me>
2013-03-26 12:52:50 -0700

    Fix allowed hosts

--- ntucker/settings.py
+++ ntucker/settings.py
@@ -25,7 +25,7 @@ DATABASES['default']['OPTIONS'] = {'autocommit': True,}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [".ntucker.me", "http://ntucker.herokuapp.com", ]
+ALLOWED_HOSTS = [".ntucker.me", "ntucker.herokuapp.com", ]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
